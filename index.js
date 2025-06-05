dec8853da328cb4c50a2701c9b3563ced0368b9b
emanuellevitorializ <169068332+emanuellevitorializ@users.noreply.github.com>
2025-06-05 18:05:59 +0000

    Aula do dia 05/06

---
+++ index.js
@@ -0,0 +1,65 @@
+const prompt = require('prompt-sync');
+const entrada = prompt();
+let nome = entrada("Digite seu nome: ");
+console.log("O nome digitado foi: " + nome);
+let idade = entrada ("Digite sua idade: ");
+console.log("A idade digitada foi %d: " + idade);
+
+console.log("✈️  Agencia de viagens Liz ✈️ ")
+
+console.log()
+
+console.log("BEM VINDO A AGÊNCIA DE VIAGENS LIZ")
+
+console.log()
+
+console.log("✨ Nossos destinos disponíveis são ✨")
+
+console.log()
+
+console.log("1° Orlando 🏰")
+
+console.log("Orlando: O Destino Mágico!  Orlando, Flórida, é famosa por seus parques temáticos como o Walt Disney World e o Universal Orlando Resort. Ele oferece diversão para toda a família, além de ótimos restaurantes e vida noturna animada.")
+console.log()
+
+console.log("2° Paris 🥐")
+
+console.log("Paris: A Cidade da Luz!  Paris, França, é conhecida por sua beleza icônica, com atrações como a Torre Eiffel, o Louvre e a Catedral de Notre-Dame. Famosa por sua cultura, gastronomia e moda, Paris encanta com seus charmosos cafés e ruas históricas. Um destino romântico e cultural imperdível!");
+
+console.log();
+
+console.log("3° Nova York 🗽")
+
+         console.log("Nova York: A grande cidade que nunca dorme! Nova York, EUA, é a cidade que nunca para. Com iconicos pontos de referencia como o Empire state Building, o time square e o museu de historia natural, Nova York oferece uma experiencia unica de diversao, cultura e entreterimento.")
+
+            console. log()
+
+console.log("4° Ilhas Maldivas 🏝️")
+
+console.log("ilhas maldivas o paraiso tropical! As ilhas maldivas são um destino de sonho,com suas águas cristalinas, praias de areia branca e resorts de luxos ofertendo uma experiencia incrivel de relaxamento e bem estar.")
+
+console.log("5° Dubai 🏜️")
+
+console.log()
+
+console.log(" Dubai: O destino de luxo e aventuras! Dubai, Emirados Árabes, uma cidade que combina mederninade e tradução, com atrações como o Burj Khalifa e o Dubai Mall. Além de ser um destino de compras e entreterimento Dubai oferece uma experiencia unica de aventura e diversão.");
+
+console.log()
+
+let login = entrada ("você deseja fazer login?")
+let senha = entrada ("digite sua senha:")
+
+let loginCerto = "admin";
+let senhaCerta = "123456";
+
+if(senha == senhaCerta ){
+
+   console.log("login feito com sucesso")
+  console.log("BEM VINDO AO SISTEMA DE ADM")
+}
+
+
+
+
+
+
